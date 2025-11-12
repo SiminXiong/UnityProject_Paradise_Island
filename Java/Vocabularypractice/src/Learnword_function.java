30b565911cf8d1011b6a5d56577466dab872a690
SiminXiong <elise.xiong@outlook.com>
2025-11-12 16:51:27 +0100

    Delete Java directory

--- Java/Vocabularypractice/src/Learnword_function.java
+++
@@ -1,6 +0,0 @@
-import java.util.HashMap;
-
-public interface Learnword_function {
-    void practice(HashMap<String,String> Vocabulary);
-    void PrintErrorList();
-}
--- Java/Vocabularypractice/src/VocabularyList_Method.java
+++
@@ -1,6 +0,0 @@
-public interface VocabularyList_Method {
-    void Initialise();
-    void ShowList();
-    void Addword();
-    void ListGermanWord();
-}
